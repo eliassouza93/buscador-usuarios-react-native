b7e4d794971b8655443659fc3211ad03014e4cc7
Elias de Souza <elias99-souza@hotmail.com>
2023-03-13 21:05:31 -0300

    projetoApp

---
+++ src/paginas/rotas.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StatusBar, SafeAreaView } from 'react-native';
+import Principal from './Principal'
+import CriarRepositorio from './CriarRepositorio'
+import InfoRepositorio from './InfoRepositorio'
+import Repositorios from './Repositorios'
+
+
+const Tab = createBottomTabNavigator()
+
+export default function Rotas() {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar />
+            <NavigationContainer>
+                <Tab.Navigator>
+                    <Tab.Screen name='Principal' component={Principal} />
+                    <Tab.Screen name='CriarRepositorio' component={CriarRepositorio} />
+                    <Tab.Screen name='InfoRepositorio' component={InfoRepositorio} />
+                    <Tab.Screen name='Repositorios' component={Repositorios} />
+                </Tab.Navigator>
+
+            </NavigationContainer>
+
+
+        </SafeAreaView>
+
+    );
+}
